5ef54bb8b79a101e4ac9273718e099604d2a9712
pyler <psycoder@pyler.tech>
2022-09-15 16:29:53 +0900

    feat: add auto-update script and delete unecessary one

--- initialize.sh
+++
@@ -1,3 +0,0 @@
-
-ln ./pylintrc ../.pylintrc
-ln ./pre-commit-config.yaml ../.pre-commit-config.yaml
---
+++ update.sh
@@ -0,0 +1,26 @@
+DIR=$PWD
+
+if [[ $DIR == *"python-tools"* ]]; then
+	cd ..
+fi
+echo "git submodule init"
+git submodule init
+echo "git submodule update"
+git submodule update
+echo "change dir into python-tools"
+cd python-tools
+
+echo "update python-tools"
+git pull origin main
+cd ..
+
+echo "remove old version of pylintrc and .pre-commit-config.yaml"
+rm -rf .pylintrc
+rm -rf .pre-commit-config.yaml
+
+echo "generate new files"
+ln -s python-tools/pylintrc .pylintrc
+ln -s python-tools/pre-commit-config.yaml .pre-commit-config.yaml
+
+echo "done"
+
